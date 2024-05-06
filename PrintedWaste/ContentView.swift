9210138d630018b7ee07e7bd6b59a2bce44c370d
Kiefer <kieferlin1001@gmail.com>
2024-05-05 20:58:27 -0700

    Initial Commit

---
+++ PrintedWaste/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PrintedWaste
+//
+//  Created by Kiefer Lin on 2024-05-05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PrintedWaste/PrintedWasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrintedWasteApp.swift
+//  PrintedWaste
+//
+//  Created by Kiefer Lin on 2024-05-05.
+//
+
+import SwiftUI
+
+@main
+struct PrintedWasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
